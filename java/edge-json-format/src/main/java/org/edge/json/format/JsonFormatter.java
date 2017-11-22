f2456b022bb48809b3cda20e2c34a62753547fa0
jeongin-kim <jeongin.kim@samsung.com>
2017-11-22 17:07:17 +0900

    Modify function name in JsonFormatter - Modify function name in JsonFormatter

--- java/edge-json-format/src/main/java/org/edge/json/format/JsonFormatter.java
+++ java/edge-json-format/src/main/java/org/edge/json/format/JsonFormatter.java
@@ -5,11 +5,11 @@ import com.google.gson.Gson;
 public class JsonFormatter {
   private static Gson gson = new Gson();
 
-  public static String changeCommandDataToJsonString(EdgeData commandData) {
+  public static String encodeEdgeDataToJsonString(EdgeData commandData) {
     return gson.toJson(commandData);
   }
 
-  public static EdgeData changeJsonStringtoCommandData(String json) {
+  public static EdgeData decodeJsonStringToEdgeData(String json) {
     return gson.fromJson(json, EdgeData.class);
   }
 }
