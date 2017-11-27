7eb8e100c4cda77d901283b01df18feb9cdcd4db
jihwan.seo <jihwan.seo@samsung.com>
2017-11-27 17:19:52 +0900

    replace class names

--- java/edge-json-format/src/main/java/org/edge/json/format/EdgeFormatIdentifier.java
+++ java/edge-json-format/src/main/java/org/edge/json/format/EdgeFormatIdentifier.java
@@ -1,15 +1,17 @@
 package org.edge.json.format;
 
-public enum FormatDefaultValue {
+public enum EdgeFormatIdentifier {
   VERSION(0, "edge-1.0", "edge-json-format version"),
   
-  OPCUA_TYPE(10, "opcua", "edge-json-format opcua protocol type");
+  OPCUA_TYPE(10, "opcua", "opcua protocol type"),
+	CNC_TYPE(11, "cnc", "CNC protocol type"),
+	PLC_TYPE(12, "cnc", "PLC protocol type");
  
   private int code;
   private String value;
   private String description;
 
-  private FormatDefaultValue(int code, String value, String description) {
+  private EdgeFormatIdentifier(int code, String value, String description) {
       this.code = code;
       this.value = value;
       this.description = description;
