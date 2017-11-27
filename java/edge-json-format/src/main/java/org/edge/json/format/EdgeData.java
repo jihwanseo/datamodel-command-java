81b1abf3034d857c5e765f2d4f966348eab73cf7
jeongin-kim <jeongin.kim@samsung.com>
2017-11-27 16:48:06 +0900

    Add DefaultValue and Protocol_Categoty - add DefaultValue - add Protocol_Category in EdgeData

--- java/edge-json-format/src/main/java/org/edge/json/format/EdgeData.java
+++ java/edge-json-format/src/main/java/org/edge/json/format/EdgeData.java
@@ -3,40 +3,20 @@ package org.edge.json.format;
 import java.util.ArrayList;
 
 public class EdgeData {
-  private static final String DEFAULT_VERSION = "json-1.0";
   private String version = null;
+  private String protocolCategory = null;
   private ArrayList<EdgeElement> edgeElements = null;
   
-  public EdgeData(String version, ArrayList<EdgeElement> edgeElements){
+  public EdgeData(String version, String protocolCategory, ArrayList<EdgeElement> edgeElements){
     this.version = version;
+    this.protocolCategory = protocolCategory;
     this.edgeElements = edgeElements; 
   }
   
-  public EdgeData(String version){
+  public EdgeData(String version, String protocolCategory){
     this.version = version;
-    edgeElements = new ArrayList<EdgeElement>();
-  }
-  
-  public EdgeData(ArrayList<EdgeElement> edgeElements){
-    this.version = DEFAULT_VERSION;
-    this.edgeElements = edgeElements;
-  }
-  
-  public EdgeData(EdgeElement edgeElement){
-    this.version = DEFAULT_VERSION;
-    edgeElements = new ArrayList<EdgeElement>();
-    edgeElements.add(edgeElement);
-  }
-  
-  public EdgeData(String version, EdgeElement edgeElement){
-    this.version = version;
-    edgeElements = new ArrayList<EdgeElement>();
-    edgeElements.add(edgeElement);
-  }
-  
-  public EdgeData(){
-    this.version = DEFAULT_VERSION;
-    edgeElements = new ArrayList<EdgeElement>();
+    this.protocolCategory = protocolCategory;
+    this.edgeElements = new ArrayList<EdgeElement>();
   }
   
   public void setVersion(String version){
@@ -54,8 +34,12 @@ public class EdgeData {
   public ArrayList<EdgeElement> getEdgeElementList(){
     return edgeElements;
   }
-
-  public void addEdgeElement(EdgeElement edgeElement){
-    edgeElements.add(edgeElement);
+  
+  public void setProtocolCategory(String protocolCategory){
+    this.protocolCategory = protocolCategory;
+  }
+  
+  public String getProtocolCategory(){
+    return protocolCategory;
   }
 }
