e69bb3a09c98257abd8a97bd4651e18cd1f6c33d
차지원/IoT Lab(SR)/Engineer/삼성전자 <jw_wonny.cha@samsung.com>
2017-12-07 16:39:24 +0900

    Update build_arm64.sh

--- command-json-format/build_arm64.sh
+++ command-json-format/build_arm64.sh
@@ -4,7 +4,7 @@ echo $PROJECT_ROOT
 
 #start package device-service
 cd $PROJECT_ROOT
-mvn clean install -U
+mvn clean install -U -Dmaven.test.skip=true
 echo "done"
 
 cp /usr/bin/qemu-aarch64-static .
