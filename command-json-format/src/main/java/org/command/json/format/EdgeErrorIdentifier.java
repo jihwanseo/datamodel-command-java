505912fd01a878083381e9be8b2c52110a935e5e
jeongin-kim <jeongin.kim@samsung.com>
2017-12-07 21:10:40 +0900

    Add EdgeErrorIdentifier.java - Add EdgeErrorIdentifier.java (Error message define)

---
+++ command-json-format/src/main/java/org/command/json/format/EdgeErrorIdentifier.java
@@ -0,0 +1,45 @@
+package org.command.json.format;
+
+public enum EdgeErrorIdentifier {
+  EDGE_DS_ERROR_RESPONSE_NULL(0, "edge_ds_error_response_null", 
+      "Edge DeviceService Response from Device is null"),
+  EDGE_DS_ERROR_RESPONSE_TIME_OUT(1, "edge_ds_error_time_out", 
+      "Edge DeviceService Response from Device is time out");
+
+  private int code;
+  private String value;
+  private String description;
+
+  private EdgeErrorIdentifier(int code, String value, String description) {
+    this.code = code;
+    this.value = value;
+    this.description = description;
+  }
+
+  /**
+   * @fn int getCode()
+   * @brief get enum code
+   * @return code
+   */
+  public int getCode() {
+    return code;
+  }
+
+  /**
+   * @fn String getValue()
+   * @brief get enum value
+   * @return value
+   */
+  public String getValue() {
+    return value;
+  }
+
+  /**
+   * @fn String getDescription()
+   * @brief get description
+   * @return description
+   */
+  public String getDescription() {
+    return description;
+  }
+}
