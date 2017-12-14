65a29e22750bba4c002106ad728d6e475efef45b
jihwan.seo <jihwan.seo@samsung.com>
2017-12-14 10:55:47 +0900

    replace build path and update document related format

--- command-json-format/src/main/java/org/command/json/format/EdgeErrorIdentifier.java
+++ command-json-format/src/main/java/org/command/json/format/EdgeErrorIdentifier.java
@@ -1,14 +1,13 @@
 package org.command.json.format;
 
 public enum EdgeErrorIdentifier {
-	EDGE_DS_ERROR_RESPONSE_NULL(0, "edge_ds_error_response_null", 
-      "Edge Device Service Response is null"),
-  EDGE_DS_ERROR_RESPONSE_TIME_OUT(1, "edge_ds_error_time_out", 
-      "Edge Device Service Response is time out"),
-  EDGE_DS_ERROR_INVALID_PARAM(2, "edge_ds_error_invalid_param", 
-	      "Edge Device Service Reuqest is invalid parameter"),
-  EDGE_DS_RESPONSE_OK(30, "edge_ds_response_ok", 
-	      "Edge Device Service Response is good");
+  EDGE_DS_ERROR_RESPONSE_NULL(0, "edge_ds_error_response_null",
+      "Edge Device Service Response is null"), EDGE_DS_ERROR_RESPONSE_TIME_OUT(1,
+          "edge_ds_error_time_out",
+          "Edge Device Service Response is time out"), EDGE_DS_ERROR_INVALID_PARAM(2,
+              "edge_ds_error_invalid_param",
+              "Edge Device Service Reuqest is invalid parameter"), EDGE_RESPONSE_OK(30,
+                  "edge_response_ok", "Edge Device Service Response is good");
 
   private int code;
   private String value;
